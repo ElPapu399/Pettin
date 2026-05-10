dee5aa67409b668450263bfe340f29349083a441
Sebastian <sebas@gmail.com>
2026-05-10 12:35:59 -0500

    Cambio en archivo Navbar

---
+++ src/components /Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="my-4 px-6">
+                <div className="grid grid-cols-3 items-center">
+                    <div className="flex justify-start">
+                        <div href="/" className="flex items-center text-gray-900 no-underline">
+                            <Link className="text-2xl font-bold text-purple-500" to="/">Pettin</Link>
+                        </div>
+                    </div>
+                    <div class="flex justify-center gap-4">
+                        <Link className="text-gray-700 hover:text-black" to="/explore">Explore</Link>
+                        <Link className="text-gray-700 hover:text-black" to="/messages">Messages</Link>
+                        <Link className="text-gray-700 hover:text-black" to="/matches">Matches</Link>
+                        <Link className="text-gray-700 hover:text-black" to="/profile">Profile</Link>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
